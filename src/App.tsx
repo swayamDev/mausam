8f43d9fcfd3cc745536354074dfc2e5727c651c3
Swayam Swarup Panda <swayamcoder@gmail.com>
2026-03-21 00:54:34 +0530

    setup: tailwind, shadcn, and typescript configuration

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
+import { Button } from "./components/ui/button";
+
 const App = () => {
-  return <div className="text-7xl text-amber-300 bg-blue-900">App</div>;
+  return (
+    <>
+      <div className="text-7xl text-amber-300 bg-blue-900">App</div>
+      <Button>Button</Button>
+    </>
+  );
 };
 
 export default App;
